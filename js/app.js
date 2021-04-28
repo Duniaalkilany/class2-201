2740e5bf857d16a0fd083f2704e148c8072d92d2
Duniaalkilany <duniaalkilany908@gmail.com>
2021-04-28 12:40:16 +0300

    editing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+let score =0; 
 
 let namee = prompt('Hello Hello , what\'s your name ?!');
 alert ('hello ' + namee + ' Welcom to my site , Enjoy !' );
@@ -16,8 +17,11 @@ switch (college.toUpperCase()) {
     case 'YES':
     case 'Y' :
         alert ('Your answer is correct ');
+        score++;
        // console.log('correct');
+       score++;
         break;
+      
     case 'NO':
     case 'N':
         alert ('Your answer is false !');
@@ -35,6 +39,7 @@ switch (color.toUpperCase()) {
         case 'Y' :
         alert ('Your answer is correct ');
       //  console.log('correct');
+      score++;
         break;
     case 'NO' :
         case 'N':
@@ -52,6 +57,7 @@ switch (hoppy.toUpperCase()) {
         case 'Y' :
         alert ('Your answer is correct ');
        // console.log('correct');
+       score++;
         break;
     case 'NO' :
         case 'N':
@@ -71,6 +77,7 @@ switch (team.toUpperCase()) {
         case 'Y' :
         alert ('Your answer is correct ');
        // console.log('correct');
+       score++;
         break;
     case 'NO' :
         case 'N':
@@ -90,6 +97,7 @@ switch (country.toUpperCase()) {
     case 'YES' :
         alert ('Your answer is correct ');
        // console.log('correct');
+       score++;
         break;
     case 'NO' :
         case 'N':
@@ -100,13 +108,14 @@ switch (country.toUpperCase()) {
         alert ('invalid , You have to answer YES or NO (Y/N) !');  
 }
 
-alert ( namee + 'Let\'s play the guessing game , answer the following sex questions with a number .let\'s gooo !!!');
+alert ( namee + 'Let\'s play the guessing game , answer this question with a number .let\'s gooo !!!');
 
 let qq1 = 0 ;
 for (let i=0 ; i<4 ; i++ ){
     let q1 = Number(prompt('guess what is the month of my birth ?'));
    if (q1 === 11 )
     { alert( 'Correct , you guessed !'); 
+    score++;
 break ;
  }
  else if  (q1 < 11)
@@ -124,113 +133,10 @@ if (qq1==4){alert (' Your chances of guessing have expired , the correct answer 
 
 
 
-let qq2 = 0 ;
-for (let i=0 ; i<4 ; i++ ){
-    let q2 = Number(prompt(' guess how old I am ?'));
-   if (q2 === 24 )
-    { alert(' Correct , you guessed !'); 
-break ;
- }
- else if  (q2 < 24)
-{alert(' wrong , too low ! ');
-qq2++;
-
-}
-else if ( q2 > 24) 
-{alert ('wrong , too high ! ');
-qq2++
- }
- 
-}
-if (qq2==4){alert ('Your chances of guessing have expired , the correct answer is 24') ;}
- 
-let qq3 = 0 ;
-for (let i=0 ; i<4 ; i++ ){
-    let q3 = Number(prompt('guess what is my high school GPA ?'));
-   if (q3 === 93 )
-    { alert('Correct , you guessed !'); 
-break ;
- }
- else if  (q3 < 93)
-{alert(' wrong , too low ! ');
-qq3++;
-
-}
-else if ( q3 > 93) 
-{alert ('wrong , too high ! ');
-qq3++
- }
- 
-}
-if (qq3==4){alert ('Your chances of guessing have expired , the correct answer is 93') ;}
 
 
-let qq4 = 0 ;
-for (let i=0 ; i<4 ; i++ ){
-    let q4 = Number(prompt('guess what is my favorite month of the year?'));
-   if (q4 === 12 )
-    { alert(' Correct , you guessed !'); 
-break ; 
- }
- else if  (q4 < 12)
-{alert(' wrong , too low ! ');
-qq4++;
+alert ('Let\'s play another game , guess what is the answer to this question , you have 6 guessing chances for each question ! ');
 
-}
-else if ( q4 > 12) 
-{alert ('wrong , too high ! ');
-qq4++
- }
- 
-}
-if (qq4==4){alert ('Your chances of guessing have expired , the correct answer is 12') ;}
-
-
-let qq5 = 0 ;
-for (let i=0 ; i<4 ; i++ ){
-    let q5 = Number(prompt('guess what my weight is?'));
-   if (q5 === 53 )
-    { alert(' Correct , you guessed !'); 
-break ;
- }
- else if  (q5< 53)
-{alert(' wrong , too low ! ');
-qq4++;
-
-}
-else if ( q5 > 53) 
-{alert (' wrong , too high ! ');
-qq5++
- }
- 
-}
-if (qq5==4){alert ('Your chances of guessing have expired , the correct answer is 53') ;}
-
-
-let qq6 = 0 ;
-for (let i=0 ; i<4 ; i++ ){
-    let q6 = Number(prompt('guess what number I\'m thinking of?'));
-   if (q6 === 100 )
-    { alert('Correct , you guessed !'); 
-break ;
- }
- else if  (q6< 100)
-{alert('wrong , too low ! ');
-qq6++;
-
-}
-else if ( q6 > 100) 
-{alert ('wrong , too high ! ');
-qq6++
- }
- 
-}
-if (qq6==4){alert ('Your chances of guessing have expired, the correct answer is 100') ;}
-
-
-alert ('Let\'s play another game , guess what is the answer to these seven questions, you have 6 guessing chances for each question , every time you will answer the correct answer , you will get one score, when you finish answering all the questions , I will show you your score!!');
-
-let score =0; 
 
 let favfood = ['MANSAF', 'BURGER', 'FISH' ];
 
@@ -251,114 +157,6 @@ if (c1==6){alert (' Your chances of guessing have expired , my favourite foods a
 
 
 
-
-let favcolor = ['RED', 'BLACK', 'BLUE' ];
-
-let c2 = 0 ;
-for (let i=0 ; i<6 ; i++ )
-{let ansnwer2 = prompt('guess one of my favourite color !' );
-ansnwer2=ansnwer2.toUpperCase();
-    if ( ansnwer2===favcolor[0] || ansnwer2===favcolor[1] || ansnwer2===favcolor[2]){
-    alert ( 'correct  ' + ansnwer2 + ' is one of my fav colors . my favourite colors are :' + favcolor );
-    score++;
-    break;
-}else { alert('wrong , guess again !');
-c2++;}
-}
-   
-    
-if (c2==6){alert (' Your chances of guessing have expired , my favourite colors are :' + favcolor) ;}
-
-
-
-let favfilm = ['TWILIGHT', 'ME BEFORE YOU', 'THE VOW' ];
-
-let c3 = 0 ;
-for (let i=0 ; i<6 ; i++ )
-{let ansnwer3 = prompt(' guess one of my fav film ?' );
-ansnwer3=ansnwer3.toUpperCase();
-    if ( ansnwer3===favfilm[0] || ansnwer3===favfilm[1] || ansnwer3===favfilm[2]){
-    alert ( ' correct  ' + ansnwer3 + ' is one of my favourite films . my favourite films are : ' + favfilm );
-    score++;
-    break;
-}else { alert('wrong , guess again !');
-c3++;}
-}
-   
-    
-if (c3==6){alert (' Your chances of guessing have expired, my favourite films are : ' + favfilm ) ;}
-
-
-
-
-
-let favsinger = ['AMR DIAB', 'FAIROUZ', 'ELISSA' ];
-
-let c4 = 0 ;
-for (let i=0 ; i<6 ; i++ )
-{let ansnwer4 = prompt('guess one my favourite singers ?' );
-ansnwer4=ansnwer4.toUpperCase();
-    if ( ansnwer4===favsinger[0] || ansnwer4===favsinger[1] || ansnwer4===favsinger[2]){
-    alert ( 'correct  ' + ansnwer4 + ' is one of my favourite singers . my favourite singers are : ' + favsinger );
-    score++;
-    break;
-}else { alert('wrong , guess again !');
-c4++;}
-}
-   
-    
-if (c4==6){alert ('Your chances of guessing have expired , my favourite singers are : ' + favsinger) ;}
-
-
-let favfootballteam = ['REAL MADRID', 'BARCELONA', 'JUVENTUS' ];
-
-let c5 = 0 ;
-for (let i=0 ; i<6 ; i++ )
-{let ansnwer5 = prompt('guess one of my fav fotball team ?' );
-ansnwer5=ansnwer5.toUpperCase();
-    if ( ansnwer5===favfootballteam[0] || ansnwer5===favfootballteam[1] || ansnwer5===favfootballteam[2]){
-    alert ( ' correct  ' + ansnwer5 + ' is one of my favourite football teams , my favourite football teams are :' + favfootballteam );
-    score++;
-    break;
-}else { alert('wrong , guess again !');
-c5++;}
-}
-   
-    
-if (c5==6){alert ('Your chances of guessing have expired, my favourite football teams  are : ' + favfootballteam) ;}
-
-let favfootballplayer = ['RONALDO', 'RAMOS', 'MARCELO' ];
-
-let c6 = 0 ;
-for (let i=0 ; i<6 ; i++ )
-{let ansnwer6 = prompt('guess one of my favourite football players ?' );
-ansnwer6=ansnwer6.toUpperCase();
-    if ( ansnwer6===favfootballplayer[0] || ansnwer6===favfootballplayer[1] || ansnwer6===favfootballplayer[2]){
-    alert ( 'correct  ' + ansnwer6 + ' is one of my favourite football players . my favourite football players are : ' + favfootballplayer );
-    score++;
-    break;
-}else { alert('wrong , guess again !');
-c6++;}
-}
-   
-    
-if (c6==6){alert ('Your chances of guessing have expired, my favourite football players are :  ' + favfootballplayer) ;}
-
-let favfruit= ['APPLE', 'ORANGE', 'BANANA' ];
-
-let c77 = 0 ;
-for (let i=0 ; i<6 ; i++ )
-{let ansnwer7 = prompt('guess one of my favourite fruits ?' );
-ansnwer7=ansnwer7.toUpperCase();
-    if ( ansnwer7===favfruit[0] || ansnwer7===favfruit[1] || ansnwer7===favfruit[2]){
-    alert ( 'correct  ' + ansnwer7 + ' is one of my favourite fruites . my favourite fruites are : ' + favfruit );
-    score++;
-    break;
-}else { alert('wrong, guess again !');
-c77++;}
-}   
-if (c77==6){alert ('Your chances of guessing have expired . my favourite fruites are :' + favfruit) ;}
-
-alert ('your score is' + score + ' out of 7');
+alert ('your score is ' +  score + '  out of 7');
 
 alert ('Thank you  ' +  namee  +  ' for visiting my website, stay well' );
